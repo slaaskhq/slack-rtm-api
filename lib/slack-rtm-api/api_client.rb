5e1982fc95ae633ce64936c338c5f742b5186bc7
Rob Zwissler <rob@zwissler.org>
2016-03-06 07:27:07 -0800

    Added IO::select around socket read to avoid blocking

--- lib/slack-rtm-api/api_client.rb
+++ lib/slack-rtm-api/api_client.rb
@@ -3,6 +3,7 @@ require 'net/http'
 require 'socket'
 require 'websocket/driver'
 require 'logger'
+include IO::WaitReadable
 
 module SlackRTMApi
 
@@ -10,9 +11,11 @@ module SlackRTMApi
     VALID_DRIVER_EVENTS = [:open, :message, :error]
     BASE_URL            = 'https://slack.com/api'
     RTM_START_PATH      = '/rtm.start'
+    IO_SELECT_TIMEOUT   = 0.001
 
-    def initialize(token: nil, silent: true)
+    def initialize(token: nil, ping_threshold: 15, silent: true)
       @token      = token
+      @ping_threshold = ping_threshold
       @silent     = silent
 
       @logger     = logger = Logger.new(STDOUT) unless silent
@@ -48,10 +51,11 @@ module SlackRTMApi
       @socket.connect
 
       @driver = WebSocket::Driver.client SlackRTMApi::ClientWrapper.new(@url.to_s, @socket)
+      @last_activity = Time.new.to_i
 
       @driver.on :open do
         @connected = true
-        @last_activity = Time.new
+        @last_activity = Time.new.to_i
         send_log "WebSocket::Driver is now connected"
         @event_handlers[:open].call unless @event_handlers[:open].nil?
       end
@@ -65,14 +69,14 @@ module SlackRTMApi
 
       @driver.on :error do |event|
         @connected = false
-        @last_activity = Time.new
+        @last_activity = Time.new.to_i
         send_log "WebSocket::Driver received an error"
         @event_handlers[:error].call unless @event_handlers[:error].nil?
       end
 
       @driver.on :message do |event|
         data = JSON.parse event.data
-        @last_activity = Time.new
+        @last_activity = Time.new.to_i
         send_log "WebSocket::Driver received an event with data: #{data}"
         if data['type'] == 'reconnect_url'
           @url = data['url']
@@ -89,7 +93,10 @@ module SlackRTMApi
     def start
       t = Thread.new do
         init
-        loop { check_ws }
+        loop do
+          check_ws
+          sleep 0.1
+        end
       end
 
       t.abort_on_exception = true
@@ -98,9 +105,16 @@ module SlackRTMApi
     private
 
     def check_ws
-      data = @socket.readpartial 4096
-      @driver.parse data unless data.nil? || data.empty?
+      if IO.select([@socket], nil, nil, IO_SELECT_TIMEOUT)
+        data = @socket.readpartial 4096
+        @driver.parse data unless data.nil? || data.empty?
+      end
       handle_events_queue
+      tdiff = Time.new.to_i - @last_activity
+      if Time.new.to_i - @last_activity > @ping_threshold
+        @driver.ping
+        @last_activity = Time.new.to_i
+      end
     end
 
     def handle_events_queue
